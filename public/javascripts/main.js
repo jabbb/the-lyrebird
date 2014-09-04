d7b289eb465669fc819a016b814ffe096b3c6c64
jabbb <bertsche>
2014-09-03 17:01:30 -0700

    l

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,106 +1,115 @@
-$(function () {
+$(function() {
 
-  var lastWord;
-  var tweet;
-  var choices;
-  var names;
-  var dates;
+    var lastWord;
+    var tweet;
+    var choices;
+    var names;
+    var dates;
 
-  function initialize() {
-  lastWord = '';
-  tweet = '';
-  choices = ['', '', '', ''];
-  $("#choice1").hide();
-  $("#choice2").hide();
-  $("#choice3").hide();
-  $("#choice4").hide();
-  $("#tweet").val('\n\n(pick a word to start your tweet)');
-  $("#tweet").css({"color":"grey"});
-  $("#count").text("0/140");
-  $("#loading").text('loading...');
+    function initialize() {
+        lastWord = '';
+        tweet = '';
+        choices = ['', '', '', ''];
+        $("#choice1").hide();
+        $("#choice2").hide();
+        $("#choice3").hide();
+        $("#choice4").hide();
+        $("#tweet").val('\n\n(pick a word to start your tweet)');
+        $("#tweet").css({
+            "color": "grey"
+        });
+        $("#count").text("0/140");
+        $("#loading").text('loading...');
 
-  $.get('/search0', function (data) {
-      data = JSON.parse(data);
+        $.get('/search0', function(data) {
+            data = JSON.parse(data);
 
-    $("#loading").hide();
-      // append data to the DOM
-      for (var i=1; i<5; i++){
-        choices[i-1] = decodeURIComponent(data['choice' + i]);
-        $("#choice" + i).text(choices[i-1]);
-          $("#choice" + i).attr('title', 'Contributed by @' + data['user' + i]);
-        $("#choice" + i).fadeIn();
-      }
-    });
-  }
+            $("#loading").hide();
+            // append data to the DOM
+            for (var i = 1; i < 5; i++) {
+                choices[i - 1] = decodeURIComponent(data['choice' + i]);
+                $("#choice" + i).text(choices[i - 1]);
+                $("#choice" + i).attr('title', 'Contributed by @' + data['user' + i]);
+                $("#choice" + i).fadeIn();
+            }
+        });
+    }
 
-  function searchTweets(n) {
-    var attempt = 1;
-    $("#choice1").hide();
-    $("#choice2").hide();
-    $("#choice3").hide();
-    $("#choice4").hide();
-    $("#loading").fadeIn();
-    $("#tweet").css({"color":"black"});
+    function searchTweets(n) {
+        var attempt = 1;
+        $("#choice1").hide();
+        $("#choice2").hide();
+        $("#choice3").hide();
+        $("#choice4").hide();
+        $("#loading").fadeIn();
+        $("#tweet").css({
+            "color": "black"
+        });
 
-    if (attempt = 1){
-    lastWord = choices[n];
-    tweet = (tweet + lastWord + ' ');
-    $("#tweet").val(tweet);
-    $("#count").text((tweet.length - 1) + '/140');
-  }
-
-    $.post('/search', {'lastWord': JSON.stringify(lastWord), 'Tlength': JSON.stringify(tweet.length)}, function (data) {
-      data = JSON.parse(data);
-
-      if (data['error'] == 1) {
-        $("#loading").text('oops! no results');
-      } else if (data['error'] == 2) {
-        $("#loading").text('hmm... something messed up');
-      } else {
-      $("#loading").hide();
-      // append data to the DOM
-      for (var i=1; i<5; i++){
-        if (data['choice' + i]) {
-          choices[i-1] = decodeURIComponent(data['choice' + i]);
-          $("#choice" + i).text(choices[i-1]);
-          $("#choice" + i).attr('title', 'Contributed by @' + data['user' + i]);
-          $("#choice" + i).fadeIn();
+        if (attempt = 1) {
+            lastWord = choices[n];
+            tweet = (tweet + lastWord + ' ');
+            $("#tweet").val(tweet);
+            $("#count").text((tweet.length - 1) + '/140');
         }
-      } 
-        /*var now = new Date();
+
+        $.post('/search', {
+            'lastWord': JSON.stringify(lastWord),
+            'Tlength': JSON.stringify(tweet.length)
+        }, function(data) {
+            data = JSON.parse(data);
+
+            if (data['error'] == 1) {
+                $("#loading").text('Huh, there were no results.');
+            } else if (data['error'] == 2) {
+                $("#loading").text('Uh oh, something messed up.');
+            } else {
+                $("#loading").hide();
+                // append data to the DOM
+                for (var i = 1; i < 5; i++) {
+                    if (data['choice' + i]) {
+                        choices[i - 1] = decodeURIComponent(data['choice' + i]);
+                        $("#choice" + i).text(choices[i - 1]);
+                        $("#choice" + i).attr('title', 'Contributed by @' + data['user' + i]);
+                        $("#choice" + i).fadeIn();
+                    }
+                }
+                /*var now = new Date();
         var date = Date.parse(data['date' + i]);
         var timeSince = (now - date)/100;
         var minSince = Math.floor(timeSince/60);
         var SecSince = timeSince - (minSince * 60);
         $("#choice" + i).attr('title', 'Contributed by \'' + data['name' + i] + '\' ' + minSince + 'minutes and ' + SecSince + ' seconds ago');*/
-      }
-    });
-  }
+            }
+        });
+    }
 
-  initialize();
-  $("#choice1").tooltip();
-  $("#choice2").tooltip();
-  $("#choice3").tooltip();
-  $("#choice4").tooltip();
-
-  // on click, run the search function
-  $("#choice1").click(function () {
-    searchTweets(0)
-  });
-  $("#choice2").click(function () {
-    searchTweets(1)
-  });
-  $("#choice3").click(function () {
-    searchTweets(2)
-  });
-  $("#choice4").click(function () {
-    searchTweets(3)
-  });
-
-  $("#postTweet").click(function () {
-    $.post('/postTweet', {'status': JSON.stringify(tweet.substr(0, 140))}, function (data) {});
     initialize();
-  });
+    $("#choice1").tooltip();
+    $("#choice2").tooltip();
+    $("#choice3").tooltip();
+    $("#choice4").tooltip();
+
+    // on click, run the search function
+    $("#choice1").click(function() {
+        searchTweets(0)
+    });
+    $("#choice2").click(function() {
+        searchTweets(1)
+    });
+    $("#choice3").click(function() {
+        searchTweets(2)
+    });
+    $("#choice4").click(function() {
+        searchTweets(3)
+    });
+
+    $("#postTweet").click(function() {
+        $.post('/postTweet', {
+            'status': JSON.stringify(tweet.substr(0, 140))
+        }, function(data) {});
+        initialize();
+    });
 
 
 });
--- routes/index.js
+++ routes/index.js
@@ -7,242 +7,169 @@ var dateList = [];
 var userList = [];
 var config = require("config.js");
 var twitter = new twit(config);
+var emoji = require('emoji');
 
-exports.index = function(req, res){
-  res.render('index', { title: "~~~~~~THE~LYREBIRD~~~~~~"});
+console.log('hi😎', emoji.unifiedToHTML('hi😎'));
+
+exports.index = function(req, res) {
+    res.render('index', {
+        title: "~~~~~~THE~LYREBIRD~~~~~~"
+    });
 };
 
-exports.ping = function(req, res){
-  res.send("pong!", 200);
+exports.ping = function(req, res) {
+    res.send("pong!", 200);
 };
 
 exports.search = function(req, res) {
-	//console.log("we got: " + req.body.lastWord);
+    //console.log("we got: " + req.body.lastWord);
 
-	// grab the request from the client
-	var word = JSON.parse(req.body.lastWord);
-	var encodedWord = encodeURIComponent(word.toLowerCase().replace(/[\.,-\/#!"\[\]?$%\^&\*;:{}=\-_`~()]/g,""));
-	var Tlength = JSON.parse(req.body.Tlength);
-	//console.log(length);
-	var finished = false;
+    // grab the request from the client
+    var word = JSON.parse(req.body.lastWord);
+    //var word = '😂😂😂';
+    var encodedWord = encodeURIComponent(word.toLowerCase().replace(/[\.,-\/#!"\[\]?$%\^&\*;:{}=\-_`~()\r?\n|\r]/g, ""));
+    console.log('encoded word: ' + encodedWord)
+    var Tlength = JSON.parse(req.body.Tlength);
+    //console.log(length);
+    var finished = false;
 
-	twitter.get('search/tweets', {q: encodedWord, lang: 'en', /*result_type: 'popular',*/ /*since: dayRange(1), */count: 100}, function(err, data) {
-			//console.log(data);
-			if (err) {
-				console.log('twit error: ' + err);
-				res.send(JSON.stringify({'error': 2}));
-			}
-			if (data) {
-			//clear wordList
-			//oldWordList = wordList;
-			wordList = [];
-			nameList = [];
-			userList = [];
-			//console.log('@@@@@: ' + wordList);
+    twitter.get('search/tweets', {
+        q: '%22' + encodedWord + '%22',
+        lang: 'en',
+        /*result_type: 'popular',*/
+        /*since: dayRange(1), */
+        count: 100
+    }, function(err, data) {
+        //console.log(data);
+        if (err) {
+            console.log('twit error: ' + err);
+            res.send(JSON.stringify({
+                'error': 2
+            }));
+        }
+        if (data) {
+            //clear wordList
+            //oldWordList = wordList;
+            wordList = [];
+            nameList = [];
+            userList = [];
+            //console.log('@@@@@: ' + wordList);
 
-				//var fnd = new RegExp(word,'i');
+            //var fnd = new RegExp(word,'i');
 
-			// find next word
-			//console.log(data.statuses.length);
-			for (var i = 0; i < data.statuses.length; i++) {
-				if (wordList.length < 4) {
-					//console.log(data.statuses[i]);
+            // find next word
+            //console.log(data.statuses.length);
+            for (var i = 0; i < data.statuses.length; i++) {
+                if (wordList.length < 4) {
+                    console.log('-->  ' + data.statuses[i].text);
 
-					var name = data.statuses[i].user.name;
-					var user = data.statuses[i].user.screen_name;
-					//var screenName = data.statuses[i].user.screen_name;
-					//var location = data.statuses[i].user.location;
-					//var date = data.statuses[i].created_at;
+                    var name = data.statuses[i].user.name;
+                    var user = data.statuses[i].user.screen_name;
+                    //var screenName = data.statuses[i].user.screen_name;
+                    //var location = data.statuses[i].user.location;
+                    //var date = data.statuses[i].created_at;
 
-	        		var status = data.statuses[i].text.split(' ');
-        			var index = -1;
-        			for (var ii = 0; ii < status.length-1; ii++) {
-        				if (word.toLowerCase().replace(/[\.,-\/#!"?$%\^&\*;:{}=\-_`~()]/g,"") == status[ii].toLowerCase().replace(/[\.,-\/#!"?$%\^&\*;:{}=\-_`~()]/g,"")) {
-        					index = ii;
-        				}
-        			}
+                    var status = data.statuses[i].text.split(' ');
+                    var index = -1;
+                    for (var ii = 0; ii < status.length - 1; ii++) {
+                        if (word.toLowerCase().replace(/[\.,-\/#!"\[\]?$%\^&\*;:{}=\-_`~()\r?\n|\r]/g, "") == status[ii].toLowerCase().replace(/[\.,-\/#!"\[\]?$%\^&\*;:{}=\-_`~()\r?\n|\r]/g, "")) {
+                            index = ii;
+                        }
+                    }
 
-	        		if (index > -1 && index < status.length /*- 1 necissary? */) {
-	            	//var newWord = status[index+1]
-	            	//console.log(Tlength + status[index+1].length);
-	            		if (check(status[index+1]) && Tlength + status[index+1].length < 140) {
-	            			wordList.push(decodeURIComponent(status[index+1].replace(/\r?\n|\r/g,"")));
-	            			nameList.push(name);
-	            			userList.push(user);
-	            			//dateList.push(date);
-	            		}
-	        		}
-	        	}
-	    	}
-	    	if (wordList.length == 0){
-			console.log('HUH? THERE WERE NO RESULTS!!!');
-			res.send(JSON.stringify({'error': 1}));
-	    	}
-	  		// send choices to client
-			res.send(JSON.stringify({
-				'choice1': wordList[0],
-				'choice2': wordList[1],
-				'choice3': wordList[2],
-				'choice4': wordList[3],/*
+                    if (index > -1 && index < status.length /*- 1 necissary? */ ) {
+                        //var newWord = status[index+1]
+                        //console.log(Tlength + status[index+1].length);
+                        if (Tlength + status[index + 1].length < 140) {
+                            wordList.push(status[index + 1].replace(/\r?\n|\r/g, ""));
+                            nameList.push(name);
+                            userList.push(user);
+                            //dateList.push(date);
+                        }
+                    }
+                }
+            }
+            if (wordList.length == 0) {
+                console.log('HUH? THERE WERE NO RESULTS!!!');
+                res.send(JSON.stringify({
+                    'error': 1
+                }));
+            }
+            // send choices to client
+            res.send(JSON.stringify({
+                'choice1': wordList[0],
+                'choice2': wordList[1],
+                'choice3': wordList[2],
+                'choice4': wordList[3],
+                /*
 				'name1': nameList[0],
 				'name2': nameList[1],
 				'name3': nameList[2],
 				'name4': nameList[3],*/
-				'user1': userList[0],
-				'user2': userList[1],
-				'user3': userList[2],
-				'user4': userList[3]
-			}));
-		} else {
-			console.log('WHAT???? THERE WAS NO DATA!!!');
-			res.send(JSON.stringify({'error': 2}));
-		}
-	})
+                'user1': userList[0],
+                'user2': userList[1],
+                'user3': userList[2],
+                'user4': userList[3]
+            }));
+        } else {
+            console.log('WHAT???? THERE WAS NO DATA!!!');
+            res.send(JSON.stringify({
+                'error': 2
+            }));
+        }
+    })
 };
 
 exports.search0 = function(req, res) {
-	//console.log(wordList);
-	wordList = [];
-	var rs = startWords[Math.floor((Math.random() * startWords.length))];
-	//console.log(rs);
-	twitter.get('search/tweets', {q:rs, lang:'en', result_type:'popular', count:50}, function(err, data) {
-		if (err){
-			console.log('twit error: ' + err);
-			res.send(JSON.stringify({'error': 2}));
-		}
-		// find first word
-		while (wordList.length <= 4){
-		for (var i = 0; i < data.statuses.length; i++) {
-        	var status = data.statuses[i].text.split(' ');
-        	if (check(status[0])) {
-	            wordList.push(status[0].replace(/\r?\n|\r/g,""));
-	            nameList.push(data.statuses[i].user.name);
-	            userList.push(data.statuses[i].user.screen_name);
-        	}
-    	}
-    }
-  		// send choices to client
-  		res.send(JSON.stringify({
-			'choice1': wordList[0],
-			'choice2': wordList[1],
-			'choice3': wordList[2],
-			'choice4': wordList[3],/*
+    //console.log(wordList);
+    wordList = [];
+    twitter.get('search/tweets', {
+        q: '%22%22',
+        lang: 'en',
+        result_type: 'popular',
+        count: 50
+    }, function(err, data) {
+        if (err) {
+            console.log('twit error: ' + err);
+            res.send(JSON.stringify({
+                'error': 2
+            }));
+        }
+        // find first word
+        for (var i = 0; i < data.statuses.length; i++) {
+            if (wordList.length <= 4) {
+                var status = data.statuses[i].text.split(' ');
+                wordList.push(status[0].replace(/\r?\n|\r/g, ""));
+                nameList.push(data.statuses[i].user.name);
+                userList.push(data.statuses[i].user.screen_name);
+            }
+        }
+        // send choices to client
+        res.send(JSON.stringify({
+            'choice1': wordList[0],
+            'choice2': wordList[1],
+            'choice3': wordList[2],
+            'choice4': wordList[3],
+            /*
 			'name1': nameList[0],
 			'name2': nameList[1],
 			'name3': nameList[2],
 			'name4': nameList[3],*/
-			'user1': userList[0],
-			'user2': userList[1],
-			'user3': userList[2],
-			'user4': userList[3]
-		}));
-	})
+            'user1': userList[0],
+            'user2': userList[1],
+            'user3': userList[2],
+            'user4': userList[3]
+        }));
+    })
 };
 
 exports.postTweet = function(req, res) {
-	// grab the tweet from the client
-	var tweet = JSON.parse(req.body.status);
-	twitter.post('statuses/update', { status: tweet }, function(err, data){
-		//console.log(data);
-		//console.log(err);
-	})
+    // grab the tweet from the client
+    var tweet = JSON.parse(req.body.status);
+    twitter.post('statuses/update', {
+        status: tweet
+    }, function(err, data) {
+        //console.log(data);
+        //console.log(err);
+    })
 };
-
-
-
-var startWords = [
-'sun',
-'wonder',
-'mac',
-'ago',
-'luck',
-'o',
-'rock',
-'sick',
-'black',
-'real',
-'yet',
-'which',
-'obama',
-'awesome',
-'little',
-'haha',
-'having',
-'thank',
-'pretty',
-'game',
-'someone',
-'school',
-'those',
-'snow',
-'twurl.nl',
-'gonna',
-'hey',
-'7',
-'many',
-'start',
-'wait',
-'while',
-'google',
-'finally',
-'everyone',
-'try',
-'30',
-'god',
-'weekend',
-'most',
-'iphone',
-'stuff',
-'around',
-'music',
-'looks',
-'may',
-'thought',
-'keep',
-'yet',
-'reading',
-'must',
-'which',
-'same',
-'real',
-'follow',
-'bit',
-'hours',
-'might',
-'actually',
-'online',
-'job'
-];
-
-var badWords = [' '];
-
-var check = function(newWord) {
-	if (badWords.indexOf(newWord) == -1 && wordList.indexOf(newWord) == -1) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
